2eb696549cd358d2b3ed13ebb9f3bdc30eb7b16e
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-08-22 11:45:16 +0500

    update

---
+++ api/handler/handler.go
@@ -0,0 +1,18 @@
+package handler
+
+import "log/slog"
+
+type MainHandler interface{
+
+}
+
+type handlerImpl struct{
+	Logger *slog.Logger
+}
+
+func NewMainHandler(logger *slog.Logger)MainHandler{
+	return &handlerImpl{
+		Logger: logger,
+	}
+}
+
