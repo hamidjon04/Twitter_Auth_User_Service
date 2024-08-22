b68731f79a0625b15afc2ecab5267cd2ad37c734
Diyorbek Ne'matov <diyorbeknematov0321@gmail.com>
2024-08-22 12:48:47 +0500

    redis uchun crudlar qo'shildi

--- model/models.go
+++ model/models.go
@@ -1,43 +1,48 @@
 package model
 
-type RegisterReq struct{
-	Email string `json:"emil"`
+type RegisterReq struct {
+	Email    string `json:"emil"`
 	Password string `json:"password"`
 }
 
-type RegisterResp struct{
-	Id string `json:"id"`
+type RegisterResp struct {
+	Id      string `json:"id"`
 	Message string `json:"message"`
 }
 
-type LoginReq struct{
-	Email string `json:"email"`
+type LoginReq struct {
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-type LogenResp struct{
+type LogenResp struct {
 	RefreshToken string `json:"refresh_token"`
-	AccessToken string `json:"access_token"`
+	AccessToken  string `json:"access_token"`
 }
 
-type LogoutReq struct{
+type LogoutReq struct {
 	Id string `json:"id"`
 }
 
-type LogoutResp struct{
+type LogoutResp struct {
 	Message string `json:"message"`
 }
 
-type ResetPassReq struct{
-	Email string `json:"email"`
+type ResetPassReq struct {
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-type ResetPassResp struct{
+type ResetPassResp struct {
 	Message string `json:"message"`
 }
 
-type ChangePassReq struct{
+type ChangePassReq struct {
 	NowPassword string `json:"now_password"`
 	NewPassword string `json:"new_password"`
+}
+
+type SuccessResponse struct {
+	Message string `json:"message"`
+	Success bool   `json:"success"`
 }
