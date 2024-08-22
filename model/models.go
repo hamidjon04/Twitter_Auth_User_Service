c62889307f401d8faa302dc023628030bc0c1037
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-08-22 11:36:38 +0500

    add auth model

---
+++ model/models.go
@@ -0,0 +1,43 @@
+package model
+
+type RegisterReq struct{
+	Email string `json:"emil"`
+	Password string `json:"password"`
+}
+
+type RegisterResp struct{
+	Id string `json:"id"`
+	Message string `json:"message"`
+}
+
+type LoginReq struct{
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LogenResp struct{
+	RefreshToken string `json:"refresh_token"`
+	AccessToken string `json:"access_token"`
+}
+
+type LogoutReq struct{
+	Id string `json:"id"`
+}
+
+type LogoutResp struct{
+	Message string `json:"message"`
+}
+
+type ResetPassReq struct{
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type ResetPassResp struct{
+	Message string `json:"message"`
+}
+
+type ChangePassReq struct{
+	NowPassword string `json:"now_password"`
+	NewPassword string `json:"new_password"`
+}
